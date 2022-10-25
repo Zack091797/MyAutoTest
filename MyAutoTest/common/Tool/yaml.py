a6fe7f9e529e3376a289db0efa1a99eb9e781475
zhou <ws948511685@126.com>
2022-10-25 14:38:50 +0800

    更新basepage基类并添加yaml操作类

---
+++ MyAutoTest/common/Tool/yaml.py
@@ -0,0 +1,7 @@
+import yaml
+
+
+class YamlTool:
+
+    pass
+
--- MyAutoTest/common/UI/basepage.py
+++ MyAutoTest/common/UI/basepage.py
@@ -1,16 +1,35 @@
-
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.action = ActionChains(self.driver)
+        self.wait = WebDriverWait(self.driver, 20)
 
     def locate(self, loc):
+        """
+        定位元素
+
+        :param loc: 列表或元组，包含定位方式和定位元素
+        :return:
+        """
         return self.driver.find_element(*loc)
 
-    def locate(self, loc, index=None):
+    def locates(self, loc, index=None):
+        """
+        定位组元素组，若index为None，则返回一组元素对象list；否则返回对应下标的单个元素对象
+
+        :param loc: 列表或元组，包含定位方式和定位元素
+        :param index: 定位组元素对象的子元素对象，默认为None
+        :return:
+        """
         if index is None:
             return self.driver.find_elements(*loc)
         else:
             return self.driver.find_elements(*loc)[index]
+
+    def move_above_element(self, loc):
+        pass
