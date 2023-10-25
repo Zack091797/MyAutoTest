a8ecacc4d2738092628a05c15bed97103d338c5b
zhou <ws948511685@126.com>
2023-10-25 09:16:23 +0800

    1.添加demo接口用例存取中间值的示例，可通过cache和yaml存取方式

--- MyAutoTest/testcases/test_api/TestApiDemo.py
+++ MyAutoTest/testcases/test_api/TestApiDemo.py
@@ -9,7 +9,7 @@ from common.Tool.yamlhelper import yamlHelper
 class TestApi(ApiCase):
 
     @pytest.mark.parametrize("get_data", yamlHelper.get_yaml_data("./testdata/test_api_demo.yaml"))
-    def test_api_demo(self, req, get_data):
+    def test_api_get_token(self, req, get_data, cache):
         req_name = get_data.get("name")
         req_method = get_data.get("request").get("method")
         req_url = get_data.get("request").get("url")
@@ -23,7 +23,14 @@ class TestApi(ApiCase):
         result = req.request(req_method, req_url, req_data)
         access_token = re.findall('"access_token":"(.*?)"', result.text)
         logHelper.info(f"获取的token： {access_token}")
+        cache.set("token", access_token)
+        cache.set("default_token", "")
+        # 写入和读取要添加一个参数，如果有值就取该值为key,如果没有没有则取全部文件内容
+        yamlHelper.set_yaml_data("./testdata/tmpdata.yaml", access_token)
 
-
-
+    def test_api_show_token(self, cache):
+        cache_token = cache.get("token", None)
+        logHelper.info(f"cache取出的token: {cache_token}")
+        yaml_token = yamlHelper.get_yaml_data("./testdata/tmpdata.yaml")
+        logHelper.info(f"yaml取出的token: {yaml_token}")
 
